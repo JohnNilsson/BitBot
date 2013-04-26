9b23e2502842647bb143475bac4e100bdc4d2b1c
John Nilsson <john@milsson.nu>
2013-04-26 02:21:30 +0200

    Play

---
+++ project/Build.scala
@@ -0,0 +1,14 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+  val appName			= "bitbot"
+  val appVersion 		= "0.2"
+  val appDependencies 	= Seq()
+
+  val mtgoxApi		= Project("bitbot-mtgoxapi", file("mtgoxapi"))
+  val bitbot		= play.Project(
+  	appName, appVersion, appDependencies, file("bitbot")
+  ).dependsOn(mtgoxApi)
+}
