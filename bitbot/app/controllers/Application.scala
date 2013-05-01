048c576e4cd0510442cdf1f3438278e8d00a45a0
John Nilsson <john@milsson.nu>
2013-05-01 14:39:14 +0200

    add missing values to data

--- bitbot/app/controllers/Application.scala
+++ bitbot/app/controllers/Application.scala
@@ -18,15 +18,23 @@ object Application extends Controller {
     if (until <= since) Stream.Empty
     else TradeHistory.getUSDTrades(since, (since + step) min until) #:: fetchTrades(since + step, until, step)
 
-  def vwap(trades: Iterable[Trade]): BigDecimal =
-    if (trades.isEmpty) 0
-    else USD.round(trades.map(t => t.amount * t.price).sum / trades.map(t => t.amount).sum)
+  def fillMissing[T](filler: T, s: Stream[Option[T]]): Stream[T] = {
+    if (s.isEmpty) Stream.Empty
+    else {
+      val next = s.head getOrElse filler
+      next #:: fillMissing(next, s.tail)
+    }
+  }
+
+  def vwap(trades: Iterable[Trade]): Option[BigDecimal] =
+    if (trades.isEmpty) None
+    else Some(USD.round(trades.map(t => t.amount * t.price).sum / trades.map(t => t.amount).sum))
 
   def trades = Action { req =>
     val since = req.getQueryString("start").get.toLong * 1000
     val until = req.getQueryString("stop").get.toLong * 1000
     val step = req.getQueryString("step").get.toLong * 1000
 
-    Ok(Json.toJson(fetchTrades(since, until, step) map vwap))
+    Ok(Json.toJson(fillMissing(BigDecimal(0), fetchTrades(since, until, step) map vwap)))
   }
 }
